a27c38570c59afbfd08a31966809c4d9a8bae97b
cesc1 <francesc.aguirre@gmail.com>
2025-02-22 03:43:04 +0100

    change name of stack_new()

--- push_swap.h
+++ push_swap.h
@@ -5,10 +5,10 @@ typedef struct s_stack
 {
 	int		num;
 	struct	s_stack	*next;
-}	t_stack
+}	t_stack;
 
 //list functions
-t_stack	*list_new(int content);
+t_stack	*stack_new(int num);
 void	stack_push(t_stack **stack, int num);
 int	stack_size(t_stack *stack);
 void	stack_pop(t_stack **stack);
