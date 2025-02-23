9c35e9b5e34343c3bf5d6e49addba0f0bd5106ec
cesc1 <francesc.aguirre@gmail.com>
2025-02-23 20:13:00 +0100

    [feat] add printing example on main

--- main.c
+++ main.c
@@ -3,5 +3,23 @@
 
 int	main(void)
 {
+	t_stack	*a;
+	t_stack	*b;
+
+	a = NULL;
+	b = NULL;
+	stack_add(&a, 1);
+	stack_add(&a, 2);
+	stack_add(&a, 3);
+	stack_print(a, b);
+	push(&a, &b, "pa");
+	stack_print(a, b);
+	push(&a, &b, "pa");
+	stack_print(a, b);
+	push(&a, &b, "pa");
+	stack_print(a, b);
+
+	stack_clear(&a);
+	stack_clear(&b);
 	return (0);
 }
